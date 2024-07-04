de06c46650acfc34e3f96797534072d5dbd3eeb5
RaphaelGuim <guimaraesraphael0203@gmail.com>
2024-07-04 13:45:21 -0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import argparse
 import warnings
 
-from langchain.chains import LLMChain
+from dotenv import load_dotenv
+from langchain.chains import LLMChain, SequentialChain
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
+
+load_dotenv()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--task", default="return a list of numbers")
@@ -14,12 +17,29 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 llm = OpenAI()
+
 code_promt = PromptTemplate(
-    template="Write a very short {language} function that will {task}",
+    template="Write a {language} function that will {task}",
     input_variables=["language", "task"],
 )
 
-code_chain = LLMChain(llm=llm, prompt=code_promt)
+code_chain = LLMChain(llm=llm, prompt=code_promt, output_key="code")
 
-result = code_chain({"language": args.language, "task": args.task})
-print(result)
+test_promt = PromptTemplate(
+    template="Write a {language} test, for the following code:\n{code}",
+    input_variables=["language", "code"],
+)
+
+test_chain = LLMChain(llm=llm, prompt=test_promt, output_key="test")
+
+chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["language", "task"],
+    output_variables=["test", "code"],
+)
+
+result = chain({"language": args.language, "task": args.task})
+print(">>>>>")
+print(result["code"])
+print(">>>>>")
+print(result["test"])
